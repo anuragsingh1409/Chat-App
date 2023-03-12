7b31a0a770e56b96c5e60590378d1d855b3e4311
anuragsingh1409 <90548858+anuragsingh1409@users.noreply.github.com>
2023-03-12 19:42:34 +0530

    final

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,28 +9,66 @@ const $messages = document.querySelector('#messages')
 
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationTemplate = document.querySelector('#location-template').innerHTML
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 
 const {username , room} = Qs.parse(location.search,{ ignoreQueryPrefix: true })
+
+const autoScroll = ()=>{
+
+    //new message
+    const $newMessage = $messages.lastElementChild
+
+    //height of new message
+    const newMessageStyles = getComputedStyle($newMessage)
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
+
+    //visible height
+    const visibleHeight = $messages.offsetHeight
+    
+    //Height of container
+    const containerHeight = $messages.scrollHeight
+
+    //how far scrolled
+    const scrollOffset  = $messages.scrollTop + visibleHeight
+
+    if(containerHeight - newMessageHeight<= scrollOffset){
+        $messages.scrollTop = $messages.scrollHeight
+    }
+}
 
 socket.on('message',(message)=>{
     console.log(message)
 
     const html = Mustache.render(messageTemplate,{
+        username: message.username,
         message : message.text,
         createdAt : moment(message.createdAt).format('h:mm A')
     })
     $messages.insertAdjacentHTML('beforeend',html)
+    autoScroll()
 })
 
 socket.on('locationMessage',(message)=>{
     console.log(message)
 
     const loc = Mustache.render(locationTemplate,{
+        username: message.username,
         url: message.url,
         createdAt : moment(message.createdAt).format('h:mm A')
   
     })
     $messages.insertAdjacentHTML('beforeend',loc)
+    autoScroll()
+})
+
+socket.on('roomData',({ room, users })=>{
+
+    const html = Mustache.render(sidebarTemplate,{
+        room,
+        users
+    })
+    document.querySelector('#sidebar').innerHTML = html
 })
 
 $messageForm.addEventListener('submit',(e)=>{
